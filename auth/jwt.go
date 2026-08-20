6465648952166777e05f64ea0a861f132a2a4750
wensboy <wendisx158@163.com>
2026-08-20 21:11:28 +0800

    feat: 初步完善 jwt 校验中间件

--- auth/jwt.go
+++ auth/jwt.go
@@ -1,1 +1,58 @@
 package auth
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo/v5"
+)
+
+type UserClaims struct {
+	UserId   int    `json:"user_id"`
+	UserName string `json:"user_name"`
+}
+
+type JwtClaims struct {
+	UserClaims
+	jwt.RegisteredClaims
+}
+
+type JwtUtil interface {
+	Encode(*JwtClaims, jwt.SigningMethod, string) (string, error)
+	Decode(string, string) (*JwtClaims, error)
+	Extract(*echo.Context) string
+}
+
+type jwtUtil struct{}
+
+func NewJwtUtil() JwtUtil {
+	return jwtUtil{}
+}
+
+func (j jwtUtil) Encode(claims *JwtClaims, method jwt.SigningMethod, secret string) (string, error) {
+	token := jwt.NewWithClaims(method, claims)
+	return token.SignedString([]byte(secret))
+}
+
+func (j jwtUtil) Decode(token string, secret string) (*JwtClaims, error) {
+	parsedToken, err := jwt.ParseWithClaims(token, &JwtClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := parsedToken.Claims.(*JwtClaims); ok && parsedToken.Valid {
+		return claims, nil
+	}
+
+	return nil, jwt.ErrSignatureInvalid
+}
+
+func (j jwtUtil) Extract(c *echo.Context) string {
+	token := c.Request().Header.Get("Authorization")
+	if token != "" {
+		if len(token) > 7 && token[:7] == "Bearer " {
+			token = token[7:]
+		}
+	}
+	return token
+}
--- auth/middleware.go
+++ auth/middleware.go
@@ -1,1 +1,54 @@
 package auth
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+	"github.com/wensboy/sss/model"
+)
+
+const (
+	VarKey_AuthEnabled  = "auth::var::enabled"
+	ToolKey_AuthSkipper = "auth::tool::skipper"
+
+	VarKey_JwtSecret  = "auth.jwt::var::secret"
+	VarKey_UserClaims = "auth.jwt::var::user_claims"
+	ToolKey_JwtUtil   = "auth.jwt::tool::jwt_util"
+)
+
+type AuthSkipper func(c *echo.Context) bool
+
+func AuthWithJwt(mc model.MiddlewareContext) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c *echo.Context) error {
+			skipper, ok := mc.Get(ToolKey_AuthSkipper).(AuthSkipper)
+			if !mc.MustGet(VarKey_AuthEnabled).(bool) || (ok && skipper(c)) {
+				return next(c)
+			}
+			jwtUtil := mc.MustGet(ToolKey_JwtUtil).(JwtUtil)
+			token := jwtUtil.Extract(c)
+			if token == "" {
+				return echo.NewHTTPError(401, "Missing Authorization header")
+			}
+			claims, err := jwtUtil.Decode(token, mc.MustGet(VarKey_JwtSecret).(string))
+			if err != nil {
+				return echo.NewHTTPError(http.StatusUnauthorized, "Invalid auth token")
+			}
+			c.Set(VarKey_UserClaims, claims.UserClaims)
+			c.Set(ToolKey_JwtUtil, jwtUtil)
+			return next(c)
+		}
+	}
+}
+
+func AuthWithSession(mc model.MiddlewareContext) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c *echo.Context) error {
+			skipper, ok := mc.Get(ToolKey_AuthSkipper).(AuthSkipper)
+			if !mc.MustGet(VarKey_AuthEnabled).(bool) || (ok && skipper(c)) {
+				return next(c)
+			}
+			return next(c)
+		}
+	}
+}
